493d1289461ea352081076dfae893f1c2bb6fb9b
MuryloG <143964033+MuryloG@users.noreply.github.com>
2024-12-01 15:13:39 -0300

    Delete cowculadora directory

--- cowculadora/src/main/java/com/cowculadora/repository/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cowculadora.repository;
-
-import com.cowculadora.model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-}
--- cowculadora/src/main/java/com/cowculadora/repository/AtendimentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cowculadora.repository;
-
-import com.cowculadora.model.Atendimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtendimentoRepository extends JpaRepository<Atendimento, Long> {
-}
